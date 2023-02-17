c720b601ca8a9086bb2c5fe70f525cc23f3c9ab6
Izzymix <mauswei@yahoo.com>
2023-02-17 14:35:59 -0800

    5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main -Entry A program that prints a line with a puts function
+ * Return: (0) 
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
