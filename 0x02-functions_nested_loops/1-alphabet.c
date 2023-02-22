7ade4b82b152526bfd0405bb5e1284fcfb8a4f8d
Izzymix <mauswei@yahoo.com>
2023-02-22 03:49:26 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print alphabet - prints alphabet in lower case
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
