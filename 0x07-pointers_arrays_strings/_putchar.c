3098e6a60c2d5bb8cfa3ee7227c7890c9d618ab0
Izzymix <mauswei@yahoo.com>
2023-03-06 12:58:47 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar
+ *
+ *
+ * Return: on success 1
+ * on error, -1 is returned,
+ *
+ */
+int _putchar(char c)
+{
+return (write (1, &c, 1));
+}
