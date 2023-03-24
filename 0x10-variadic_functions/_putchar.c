a213f4b129a1bebcf05aebd2340ae3222c81aa74
Izzymix <mauswei@yahoo.com>
2023-03-24 13:50:57 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#inclide "main.h"
+
+/*
+* _pucthar - write the character
+* *@c : The character to print
+* 
+* Return 0: on success 1
+* on error -1 is returned and error no is set appropriately.
+*/
+
+int _putchar(char)
+{
+return (write (1, &c, 1))
+}
